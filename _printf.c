3e961da1d7fc6062dcffc7311a5d3e98db133c91
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-16 14:50:08 +0000

    ad functions to print input in stdout

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+  * _printf - function that prints formatted output in the stdout
+  * @format: pointer to a character.
+  *
+  * Return: int
+  */
+int _printf(const char *format, ...)
+{
+	va_list op;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	va_start(op, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			j += _putchar(format[i]);
+		}
+		else if (format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+				j += _putchar(va_arg(op, int));
+			else
+				j += print_string(va_arg(op, char *));
+		}
+		i++;
+	}
+	va_end(op);
+	return (j);
+}
